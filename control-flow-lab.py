036a09265d7fe124fcf4ec3ba5202fa9761cfa45
Is an English Major <Najeebihesa@gmail.com>
2025-08-24 15:44:40 +0300

    finish

--- control-flow-lab.py
+++ control-flow-lab.py
@@ -146,12 +146,12 @@
 def fizz_buzz():
     # Your loop and condition logic goes here
     for num in range(1, 50):
-        if num%3 ==0:
+        if num%3 ==0 and num%5 ==0:
+            print('fizzbuzz')
+        elif num%3 ==0:
             print('fiz')
         elif num%5 ==0:
             print('buzz')
-        elif num%3 ==0 and num%5 ==0:
-            print('fizzbuzz')
 
 
 # Call the function
